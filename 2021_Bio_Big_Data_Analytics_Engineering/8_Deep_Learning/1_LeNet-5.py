b9e3a797f69b68983f422219ea3c373c42c37e55
Jihwan Kim <raptorwoo@gmail.com>
2022-06-11 21:30:49 +0900

    modify gpu checking script

--- 2021_Bio_Big_Data_Analytics_Engineering/8_Deep_Learning/1_LeNet-5.py
+++ 2021_Bio_Big_Data_Analytics_Engineering/8_Deep_Learning/1_LeNet-5.py
@@ -6,11 +6,9 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from tensorflow.keras.optimizers import Adam
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train = x_train.reshape(60000, 28, 28, 1)
-x_test = x_test.reshape(10000, 28, 28, 1)
-x_train = x_train.astype(np.float32)/255.0
-x_test = x_test.astype(np.float32)/255.0
-y_train = tf.keras.utils.to_categorical(y_train, 10)
+x_train = x_train.reshape(60000, 784)
+x_test = x_test.reshape(10000, 784)
+y_train = tf.keras.utils.to_categorical(y_train, 10) # one-hot encoding
 y_test = tf.keras.utils.to_categorical(y_test, 10)
 
 # LeNet-5 model
