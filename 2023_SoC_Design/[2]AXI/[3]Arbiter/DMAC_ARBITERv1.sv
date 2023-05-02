78b60418a9c82c0974b39618fa7e841302755aed
Jihwan <raptorwoo@gmail.com>
2023-05-02 18:06:25 +0900

    fix FSM bug for arbiter v1

--- 2023_SoC_Design/[2]AXI/[3]Arbiter/DMAC_ARBITERv1.sv
+++ 2023_SoC_Design/[2]AXI/[3]Arbiter/DMAC_ARBITERv1.sv
@@ -17,18 +17,18 @@ module DMAC_ARBITER
     output  reg     [DATA_SIZE-1:0] dst_data_o
 );          
     
-    localparam      S_IDLE = 3'd0,
-                    S_READ = 3'd1,
-                    S_WREQ = 3'd2,
-                    S_WRITE = 3'd3;
+    localparam      S_READ     = 3'd0,
+                    S_RDCK     = 3'd1,
+                    S_WREQ     = 3'd2,
+                    S_WRITE    = 3'd3;
 
     /*
      *  register update for each clock 
      */ 
-    reg [2:0] state, state_n; 
+    reg [3:0] state, state_n; 
     always_ff @(posedge clk) begin
         if(!rst_n) begin 
-            state   <= S_IDLE;
+            state   <= S_READ;
         end
         else begin
             state   <= state_n;
@@ -38,67 +38,56 @@ module DMAC_ARBITER
     /* 
      *   Finite State Machine
      */ 
+    reg[2:0] choose_ch;
     reg[2:0] i, j, k, m, n; 
-    reg [DATA_SIZE-1:0] buffer[N_MASTER];
+    reg [DATA_SIZE-1:0] buffer;
     reg buffer_full[N_MASTER];
 
     always_comb begin
         state_n = state; 
         case(state) 
-            S_IDLE: begin    // take in valid input first into the buffer 
-                for(i=0; i<N_MASTER; i=i+1) begin
-                    if(src_valid_i[i]) begin
-                        buffer[i] = src_data_i[i];    // fill data into buffer
-                        buffer_full[i] = 1'b1;    // mark buffer as full 
-                        state_n = S_READ;
-                    end else begin   // stay in the state
-                        buffer_full[i] = 1'b0;  
-                    end
+            // READ STATE:::takes in valid input first into the buffer 
+            S_READ: begin   
+                if(src_valid_i[0]) begin
+                    buffer = src_data_i[0]; 
+                    choose_ch = 3'd0;
+                end else if (src_valid_i[1]) begin
+                    buffer = src_data_i[1];
+                    choose_ch = 3'd1;
+                end else if (src_valid_i[2]) begin
+                    buffer = src_data_i[2];
+                    choose_ch = 3'd2; 
+                end else if (src_valid_i[3]) begin
+                    buffer = src_data_i[3]; 
+                    choose_ch = 3'd3;
+                end else begin 
                 end
+                if(src_valid_i[0] || src_valid_i[1] || src_valid_i[2] || src_valid_i[3]) begin
+                    state_n = S_RDCK;
+                end else begin
+                end
+                
             end
-            // OUTPUT:: src_ready_o = 1
-            S_READ: begin    // signals ready to receive and fill buffers
-                for(j=0; j<N_MASTER; j=j+1) begin
-                    if(src_valid_i[j]) begin
-                        src_ready_o[j] = 1'b1;
-                    end
-                end
+            // READCHECK STATE:::signals ready to receive and fill buffers 
+            S_RDCK: begin 
+                src_ready_o[choose_ch] = 1'b1;
                 state_n = S_WREQ;
             end
-            // signals ready to send
-            S_WREQ: begin    // OUTPUT:: src_ready_o = 0, dst_valid_o = 1
-                for(k=0; k<N_MASTER; k=k+1) begin    // ready 
-                    src_ready_o[k] = 1'b0;
-                    if(buffer_full[k]) begin
-                        dst_valid_o = 1'b1;
-                        state_n = S_WRITE;
-                    end else begin
-                    end
-                end
+            // WRITE REQUEST STATE::: drives data and valid to signal that data can be sent
+            S_WREQ: begin    // signals 
+                src_ready_o[choose_ch] = 1'b0;
+                dst_valid_o = 1'b1; 
+                dst_data_o = buffer;
+                state_n = S_WRITE;
             end
-            // send everything sequentially 
-            S_WRITE: begin // OUTPUT:: 
-                if(buffer_full[0] && dst_valid_o && dst_ready_i) begin
-                    dst_data_o = buffer[0]; 
-                    buffer_full[0] = 1'b0;
-                end else if(buffer_full[1] && dst_valid_o && dst_ready_i) begin
-                    dst_data_o = buffer[1];
-                    buffer_full[1] = 1'b0;
-                end else if(buffer_full[2] && dst_valid_o && dst_ready_i) begin
-                    dst_data_o = buffer[2];
-                    buffer_full[2] = 1'b0;
-                end else if(buffer_full[3] && dst_valid_o && dst_ready_i) begin
-                    dst_data_o = buffer[3];
-                    buffer_full[3] = 1'b0;
+            // WRITE STATE::: data sent and data no longer valid. move back to first state
+            S_WRITE: begin 
+                if(dst_ready_i) begin
+                    dst_valid_o = 1'b0;
+                    state_n = S_READ; 
                 end else begin
                 end
-
-                if(!buffer_full[0] && !buffer_full[1] && !buffer_full[2] && !buffer_full[3]) begin
-                    state_n = S_IDLE;
-                end else begin
-                end
-            end
-
+            end    
         endcase 
     end 
 endmodule
