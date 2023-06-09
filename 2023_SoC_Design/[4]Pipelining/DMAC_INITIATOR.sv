4379ba6385911cffd609a9b3b340cf11962e1f88
Jihwan <raptorwoo@gmail.com>
2023-06-09 15:33:24 +0900

    fix address read for pipeline

--- 2023_SoC_Design/[4]Pipelining/DMAC_INITIATOR.sv
+++ 2023_SoC_Design/[4]Pipelining/DMAC_INITIATOR.sv
@@ -3,7 +3,7 @@
 //
 // Authors:
 // - Jungrae Kim <dale40@skku.edu>
-module DMAC_INITIATOR 
+module DMAC_ENGINE 
 (
     input   wire                clk,
     input   wire                rst_n,
@@ -60,10 +60,10 @@ module DMAC_INITIATOR
     
     reg     [2:0]       state,      state_n; 
     
-    reg     [31:0]      src_addr,   src_addr_n; 
-    reg     [31:0]      dst_addr,   dst_addr_n; 
-    reg     [15:0]      cnt,        cnt_n; 
-    reg     [3:0]       wcnt,       wcnt_n; 
+    reg     [31:0]      src_addr,                  src_addr_n; 
+    reg     [31:0]      dst_addr,                  dst_addr_n; 
+    reg     [15:0]      cnt,                       cnt_n; 
+    reg     [3:0]       wcnt,                      wcnt_n; 
 
     reg                 arvalid,
                         rready,
@@ -71,97 +71,113 @@ module DMAC_INITIATOR
                         wvalid,
                         wlast,              
                         done;
-
-    /********************************************************/
-    /************************ CFG  **************************/
-    /********************************************************/
-    /* Register updates for each clock */ 
-    always_ff @(posedge clk)
-        if(!rst_n) begin
-            state        <=    S_IDLE; 
-            
-            src_addr     <=    32'd0;
-            dst_addr     <=    32'd0; 
-            cnt          <=    16'd0; 
-
-            wcnt         <=    4'd0; 
-        end else begin 
-            state        <=    state_n; 
-
-            src_addr     <=    src_addr_n; 
-            dst_addr     <=    dst_addr_n;
-            cnt          <=    cnt_n; 
-
-            wcnt         <=    wcnt_n; 
-        end
-
-    /********************************************************/
-    /************************ FSM  **************************/
-    /********************************************************/
-    /*  Finite State Machine for Initiator  */
-    always_comb begin 
-        state_n          =    state; 
-        
-        src_addr_n       =    src_addr; 
-        dst_addr_n       =    dst_addr; 
-        cnt_n            =    cnt; 
-        wcnt_n           =    wcnt; 
-
-        case (state)
-            // Start operation when configuration register (CFG) set 
-            S_IDLE: begin
-                done          = 1'b1; 
-                if(start_i & byte_len_i != 16'd0) begin 
-                    src_addr_n           = src_addr_i; 
-                    dst_addr_n           = dst_addr_i; 
-                    cnt_n                = byte_len_i; 
-
-                    state_n              = S_BUSY; 
-                end
-            end
-            
-            // Start the pipelining                                                      
-            S_BUSY: begin 
-                // AR CHANNEL handshake incurs  increment source address 
-                if(arvalid && arready_i) begin
-                    src_addr_n     = src_addr + 'd64; 
-                end 
-                // R CHANNEL handshake incurs AW generation 
-                if(rvalid_i && rready && awvalid && awready_i) begin 
-                    dst_addr_n     = dst_addr + 'd64; 
-                    if(cnt_n > 'd64) begin
-                        cnt_n      = cnt_n - 'd64;
-                    end 
-                    else begin 
-                        cnt_n      = 0; 
-                    end 
-                end
-            end
-            
-            // When Writing Done, a request will be received through B channel
-            S_WAIT: begin
-                // TODO: Implement 
-            end
-        endcase 
-    end 
-
-    /********************************************************/
-    /**************** Pipelining ****************************/
-    /********************************************************/
     // Variable for R to W FIFO
     wire                r_to_w_fifo_full;              
     wire                r_to_w_fifo_empty;             
     wire                r_to_w_fifo_wren;              
     wire                r_to_w_fifo_rden;             
-    wire    [31:0]      r_to_w_fifo_rdata;            
+    wire    [31:0]      r_to_w_fifo_rdata;   
+    reg     [5:0]       r_to_w_fifo_count,         r_to_w_fifo_count_n;  
 
-    // Variable for Initiator to AW FIFO 
-    wire                init_to_aw_fifo_full;          // to sender 
-    wire                init_to_aw_fifo_empty;         // to receiver
-    wire                init_to_aw_fifo_wren;          // from sender
-    wire                init_to_aw_fifo_rden;          // from receiver
-    wire    [31:0]      init_to_aw_fifo_rdata;         // to receiver
+            
+    reg                 r_to_w_fifo_readflag;      // notifies if rlast has occured  
+    reg                 r_to_w_fifo_writeflag;     // notifies if wlast has occured  
 
+    // Channel Flags : activated during handshake 
+    reg                 cfgflag;
+    reg                 arflag;
+    reg                 rflag; 
+    reg                 awflag;
+    reg                 wflag;
+
+    /********************************************************/
+    /************************ CFG  **************************/
+    /********************************************************/
+    /* Register updates for each clock */ 
+    always_ff @(posedge clk) begin
+        if(!rst_n) begin
+            state                     <=    S_IDLE; 
+            
+            src_addr                  <=    32'd0;
+            dst_addr                  <=    32'd0; 
+            cnt                       <=    16'd0; 
+
+            wcnt                      <=    4'd0; 
+        end 
+        else begin 
+            state                     <=    state_n; 
+
+            src_addr                  <=    src_addr_n; 
+            dst_addr                  <=    dst_addr_n;
+            cnt                       <=    cnt_n; 
+
+            wcnt                      <=    wcnt_n;
+        end
+    end
+
+    /********************************************************/
+    /************************ FSM  **************************/
+    /********************************************************/
+    /*  Finite State Machine for Initiator  */
+    /*  Does most of acccounting oeprations */
+    always_comb begin 
+        state_n                 = state;
+
+        src_addr_n              = src_addr;
+        dst_addr_n              = dst_addr;
+        cnt_n                   = cnt;
+        wcnt_n                  = wcnt;
+
+        case (state) 
+            // Start operation when configuration register (CFG) set 
+            S_IDLE: begin
+                if (start_i & byte_len_i!=16'd0) begin
+                    src_addr_n              = src_addr_i;
+                    dst_addr_n              = dst_addr_i;
+                    cnt_n                   = byte_len_i;
+
+                    state_n                 = S_BUSY;
+                end
+            end
+
+            // Start the pipelining 
+            S_BUSY: begin
+                // AR Channel
+                if(arvalid && arready_i) begin
+                    src_addr_n                 = src_addr + 'd64; 
+                end 
+                // R CHANNEL handshake incurs address accounting operations  
+                if(rvalid_i && rready) begin 
+                    dst_addr_n                 = dst_addr + 'd64; 
+                    wcnt_n                     = awlen_o;
+                    if(cnt_n > 'd64) begin
+                        cnt_n                  = cnt_n - 'd64;
+                    end 
+                    else begin 
+                        cnt_n                  = 0; 
+                    end 
+                end 
+                // W channel handshake incurs decrementing write count 
+                if(wready_i && wvalid) begin
+                    if(wlast) begin
+                        if(cnt == 16'd0) begin
+                            state_n            = S_WAIT;
+                        end     
+                    end
+                    else begin
+                        wcnt_n                 = wcnt - 4'd1; 
+                    end
+                end
+            end
+            // When Writing Done, a request will be received through B channel
+            S_WAIT: begin
+            end
+        endcase
+    end 
+
+    /********************************************************/
+    /**************** Pipelining ****************************/
+    /********************************************************/
     /* FIFO for R to W Channel (Writing Data) */
     DMAC_FIFO #(
         .DEPTH_LG2               (4),
@@ -179,75 +195,103 @@ module DMAC_INITIATOR
         .rden_i                 (r_to_w_fifo_rden),
         .rdata_o                (r_to_w_fifo_rdata)
     );
-    /* FIFO for initiator to AW channel (generated Write Address)*/
-    DMAC_FIFO #(
-        .DEPTH_LG2               (4),
-        .DATA_WIDTH              ($bits(awaddr_o))
-    ) u_init_to_aw_fifo
-    (
-        .clk                    (clk),
-        .rst_n                  (rst_n), 
-
-        .full_o                 (init_to_aw_fifo_full), 
-        .wren_i                 (init_to_aw_fifo_wren),
-        .wdata_i                (dst_addr),
-
-        .empty_o                (init_to_aw_fifo_empty),
-        .rden_i                 (init_to_aw_fifo_rden),
-        .rdata_o                (init_to_aw_fifo_rdata)
-    );
     
     /* Control Wiring for FIFO                                                                             */
     /* change in AW, R, W channel valid/ready automatically adjusts the FIFO read enable and write enables */
     assign              r_to_w_fifo_wren         = rvalid_i & !r_to_w_fifo_full;      
-    assign              r_to_w_fifo_rden         = (wvalid & wready_i) & !r_to_w_fifo_empty; 
-    assign              init_to_aw_fifo_wren     = awvalid & !init_to_aw_fifo_full;                  // manipulate write enable 
-    assign              init_to_aw_fifo_rden     = 1'b0 & !init_to_aw_fifo_empty;                                             // manipulate read enable     
-      
-    /* Manipulation of declared FIFOs for pipelining */
+    assign              r_to_w_fifo_rden         = (wvalid & wready_i) & !r_to_w_fifo_empty;   
+
+    /* Manipulation of declared FIFOs for pipelining  */
+    /* does most of signal driving operations         */
     always_ff @(posedge clk) begin
-        // Send out AR Request 
-        if(state == S_BUSY && !r_to_w_fifo_full) begin
-            arvalid             <= 1'b1;
-            if(arvalid && arready_i) begin
-                arvalid         <= 1'b0;
-            end
+        // reset signal
+        if(!rst_n) begin
+            arvalid                   <= 1'b0;
+            rready                    <= 1'b0;
+            awvalid                   <= 1'b0;
+            wvalid                    <= 1'b0;
+            wlast                     <= 1'b0;
+            done                      <= 1'b0;
+
+            cfgflag                   <= 1'b0;
+            arflag                    <= 1'b0;
+            rflag                     <= 1'b0;
+            awflag                    <= 1'b0;
+            wflag                     <= 1'b0;
         end
-        else begin
-            arvalid             <= 1'b0;
-        end
-    
-        // Receive R 
-        // When R received, save R data in FIFO
-        // Contains AW Generation Control
-        if(state == S_BUSY && rvalid_i) begin
-            rready              <= 1'b1; 
-            if(rvalid_i && rready) begin
-                rready          <= 1'b0;
-                // START AW GENERATION
-                awvalid         <= 1'b1;
-                if(awvalid && awready_i) begin
-                    awvalid     <= 1'b0; 
+        // reset signal off 
+        else begin                   
+            // IDLE state
+            if(state == S_IDLE)begin
+                done <= 1'b1;
+                if(start_i && (byte_len_i != 16'd0)) begin
+                    cfgflag <= 1'b1;
                 end
             end
-        end
-        else begin
-            rready              <= 1'b0; 
-        end
-
-        // Send W
-        // When u_r_to_w_fifo has values, read out the value of the write 
-        if(!r_to_w_fifo_empty) begin
-            wvalid            <= 1'b1; 
-            if(wvalid && wready_i) begin
-                wvalid        <= 1'b0; 
+        
+            // Address read per one read 
+            // cfgflag   : ____________--------------
+            // arvalid_o : ______________------------
+            // arready_i : __________________--------
+            if (state == S_BUSY && cfgflag) begin
+                arvalid             <= 1'b1;
+                if(arvalid && arready_i) begin
+                    cfgflag             <= 1'b0;
+                    arvalid <= 1'b0;
+                end
+            end 
+            else begin
+                arvalid             <= 1'b0;
             end
 
+            // Receive R 
+            // When R received, save R data in FIFO
+            // Contains AW Generation Control
+            if(state == S_BUSY && rvalid_i) begin
+                rready  <= 1'b1; 
+                rflag <= 1'b1; 
+                if(rvalid_i && rready && !r_to_w_fifo_full) begin  // read channel valid-ready handshake occurs depending on the FIFO condition 
+                    rready <= 1'b0; 
+                    // handshake also lets AW generation by turning on readflag 
+                    rflag  <= 1'b1; 
+                end
+            end
+            else begin
+                rready              <= 1'b0; 
+            end
+            // AW GENERATION
+            // rflag       : _______------------------
+            // awvalid_o   : ________---------________
+            // awready_i   : ______________---________
+            if(state == S_BUSY && rflag) begin
+                awvalid                   <= 1'b1; 
+                if(awvalid && awready_i) begin
+                    awvalid               <= 1'b0; 
+                    rflag                 <= 1'b0;
+                end
+            end
+            else begin
+                awvalid                   <= 1'b0; 
+            end
+            // Write on W channel 
+            // When u_r_to_w_fifo has values, read out the value of the write 
+            // wdata_o    : --data--________--data--_______
+            // wvalid_o   : -------------------------------
+            // wready_i   : ______--______--______--_______
+            if(state == S_BUSY && !r_to_w_fifo_empty) begin
+                wvalid                        <= 1'b1; 
+                if(wvalid && wready_i && !r_to_w_fifo_empty) begin
+                    wlast                     <= (wcnt == 4'd0);
+                end
+            end
+            else begin
+                wvalid            <= 1'b0;
+            end   
+
+            // Wait 
         end
     end
-
-
-
+   
     /********************************************************/
     /**************** Output Assignments ********************/
     /********************************************************/
@@ -261,7 +305,7 @@ module DMAC_INITIATOR
     // R Channel
     assign rready_o     = rready & !r_to_w_fifo_full; 
     // AW Channel 
-    assign awaddr_o     = init_to_aw_fifo_rdata;
+    assign awaddr_o     = dst_addr;
     assign awlen_o      = (cnt >= 'd64) ? 4'hF : cnt[5:2]-4'h1; 
     assign awsize_o     = 3'b010;                                  // 4 bytes per transfer
     assign awburst_o    = 2'b01;                                   // incremental
@@ -278,3 +322,4 @@ module DMAC_INITIATOR
 
 endmodule
 // dmac_initiator written by Jihwan Kim
+
