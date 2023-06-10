3a6349eeecd731dbc0e532b3cd21510eec5081f3
Jihwan Kim <raptorwoo@gmail.com>
2023-06-10 20:10:16 +0900

    structure FSM for pipeline and fix done signal

--- 2023_SoC_Design/[4]Pipelining/DMAC_INITIATOR.sv
+++ 2023_SoC_Design/[4]Pipelining/DMAC_INITIATOR.sv
@@ -3,7 +3,7 @@
 //
 // Authors:
 // - Jungrae Kim <dale40@skku.edu>
-module DMAC_ENGINE 
+module DMAC_INITIATOR 
 (
     input   wire                clk,
     input   wire                rst_n,
@@ -56,7 +56,7 @@ module DMAC_ENGINE
     /* FIXME: Write your code here (You may use FSM implementation here) */ 
     localparam          S_IDLE    = 2'd0,
                         S_BUSY    = 2'd1,
-                        S_WAIT    = 2'd3; 
+                        S_WAIT    = 2'd2; 
     
     reg     [2:0]       state,      state_n; 
     
@@ -71,20 +71,21 @@ module DMAC_ENGINE
                         wvalid,
                         wlast,              
                         done;
+
+                        
     // Variable for R to W FIFO
     wire                r_to_w_fifo_full;              
     wire                r_to_w_fifo_empty;             
     wire                r_to_w_fifo_wren;              
     wire                r_to_w_fifo_rden;             
     wire    [31:0]      r_to_w_fifo_rdata;   
-    reg     [5:0]       r_to_w_fifo_count,         r_to_w_fifo_count_n;  
+    reg     [5:0]       r_to_w_fifo_cnt;
+
+    reg                 r_to_w_fifo_pop;
 
     // Channel Flags : activated during handshake 
-    reg                 cfgflag;
     reg                 arflag;
-    reg                 rflag; 
     reg                 awflag;
-    reg                 wflag;
 
     /********************************************************/
     /************************ CFG  **************************/
@@ -115,7 +116,7 @@ module DMAC_ENGINE
     /************************ FSM  **************************/
     /********************************************************/
     /*  Finite State Machine for Initiator  */
-    /*  Does most of acccounting oeprations */
+    /*  Does most of acccounting operations */
     always_comb begin 
         state_n                 = state;
 
@@ -128,11 +129,11 @@ module DMAC_ENGINE
             // Start operation when configuration register (CFG) set 
             S_IDLE: begin
                 if (start_i & byte_len_i!=16'd0) begin
-                    src_addr_n              = src_addr_i;
-                    dst_addr_n              = dst_addr_i;
-                    cnt_n                   = byte_len_i;
+                    src_addr_n = src_addr_i;
+                    dst_addr_n = dst_addr_i;
+                    cnt_n = byte_len_i;
 
-                    state_n                 = S_BUSY;
+                    state_n = S_BUSY;
                 end
             end
 
@@ -140,33 +141,35 @@ module DMAC_ENGINE
             S_BUSY: begin
                 // AR Channel
                 if(arvalid && arready_i) begin
-                    src_addr_n                 = src_addr + 'd64; 
+                    src_addr_n = src_addr + 'd64; 
                 end 
                 // AW CHANNEL handshake incurs address accounting operations  
                 if(awvalid && awready_i) begin 
-                    dst_addr_n                 = dst_addr + 'd64; 
-                    wcnt_n                     = awlen_o;
+                    dst_addr_n = dst_addr + 'd64; 
+                    wcnt_n = awlen_o;
                     if(cnt_n > 'd64) begin
-                        cnt_n                  = cnt_n - 'd64;
+                        cnt_n = cnt_n - 'd64;
                     end 
                     else begin 
-                        cnt_n                  = 0; 
+                        cnt_n = 0; 
                     end 
                 end 
+
                 // W channel handshake incurs decrementing write count 
                 if(wready_i && wvalid) begin
                     if(wlast) begin
                         if(cnt == 16'd0) begin
-                            state_n            = S_WAIT;
+                            state_n = S_WAIT;
                         end     
                     end
                     else begin
-                        wcnt_n                 = wcnt - 4'd1; 
+                        wcnt_n = wcnt - 4'd1; 
                     end
                 end
-            end
-            // When Writing Done, a request will be received through B channel
-            S_WAIT: begin
+
+                if(wlast && (cnt == 0)) begin
+                    state_n = S_IDLE;
+                end
             end
         endcase
     end 
@@ -194,8 +197,8 @@ module DMAC_ENGINE
     
     /* Control Wiring for FIFO                                                                             */
     /* change in AW, R, W channel valid/ready automatically adjusts the FIFO read enable and write enables */
-    assign              r_to_w_fifo_wren         = rvalid_i & !r_to_w_fifo_full;      
-    assign              r_to_w_fifo_rden         = (wvalid & wready_i) & !r_to_w_fifo_empty;   
+    assign  r_to_w_fifo_wren         = (rvalid_i & rready_o) & !r_to_w_fifo_full;      
+    assign  r_to_w_fifo_rden         = (((wvalid & wready_i) & (!r_to_w_fifo_empty)));   
 
     /* Manipulation of declared FIFOs for pipelining  */
     /* does most of signal driving operations         */
@@ -209,33 +212,35 @@ module DMAC_ENGINE
             wlast                     <= 1'b0;
             done                      <= 1'b0;
 
-            cfgflag                   <= 1'b0;
+            arflag                    <= 1'b0;
             awflag                    <= 1'b0;
-            wflag                     <= 1'b0;
+            
+            r_to_w_fifo_cnt           <= 5'b0;
+            r_to_w_fifo_pop           <= 1'b0;
         end
         // reset signal off 
         else begin                   
             // IDLE state
             if(state == S_IDLE)begin
                 done <= 1'b1;
-                if(start_i && (byte_len_i != 16'd0)) begin
-                    cfgflag <= 1'b1;
+                if(start_i) begin
+                    done <= 1'b0;
+                    arflag <= 1'b1;
                 end
             end
         
-            // Address read 
-            // cfgflag   : ______----------------____
-            // arvalid_o : _________------------_____
-            // arready_i : ____________--------_____
-            if (state == S_BUSY && cfgflag) begin
-                arvalid <= 1'b1;
+            // AR 
+            // arflag    : ______----------_________
+            // arvalid_o : _________-------_________
+            // arready_i : ______________--_________
+            if (state == S_BUSY && arflag) begin  // BUSY state and Done means its 
+                arvalid = 1'b1;
                 if(arvalid && arready_i) begin
-                    cfgflag <= 1'b0;
                     arvalid <= 1'b0;
+                    arflag <= 1'b0;
                 end
             end 
-            else begin
-                arvalid             <= 1'b0;
+            else begin // dummy to prevent latch
             end
 
             // Receive R 
@@ -244,45 +249,63 @@ module DMAC_ENGINE
             if( (state == S_BUSY) && rvalid_i) begin
                 rready  <= 1'b1; 
                 // handshake
-                if(rvalid_i && rready && !r_to_w_fifo_full) begin  // read channel valid-ready handshake occurs depending on the FIFO condition  
+                if(rvalid_i && rready && !r_to_w_fifo_full)begin  // read channel valid-ready handshake occurs depending on the FIFO condition  
                     rready <= 1'b0; 
+                    r_to_w_fifo_cnt <= r_to_w_fifo_cnt + 'd1;
                     // Start AW Generation 
                     // AW GENERATION
                     // !awflag     : -----------------_______
                     // awvalid_o   : ________---------_______
                     // awready_i   : ______________---_______
-                    if(!awflag) begin 
-                        awvalid  <= 1'b1;
-                        if(awvalid && awready_i && !awflag) begin
-                            awflag <= 1'b1;
-                            awvalid <= 1'b0;
-                        end
+                    awvalid <= 1'b1;
+                    if(awvalid && awready_i) begin
+                        awvalid <= 1'b0;
                     end
                 end
             end
             else begin
-                rready              <= 1'b0; 
+                rready <= 1'b0; 
             end
 
             // Write on W channel 
             // When u_r_to_w_fifo has values, read out the value of the write 
-            // wdata_o    : --data--________--data--_______
+            // wdata_o    : --data--________-data-_______
             // wvalid_o   : -------------------------------
-            // wready_i   : ______--______--______--_______
-            if( (state == S_BUSY) && !r_to_w_fifo_empty) begin
-                wvalid                        <= 1'b1; 
+            // wready_i   : ______--____________--_______
+            if( (state == S_BUSY) && (r_to_w_fifo_cnt > 5'd1)) begin
+                wvalid <= 1'b1; 
+                // handshake
                 if(wvalid && wready_i) begin
-                    wlast <= (wcnt == 4'd0);
+                    r_to_w_fifo_cnt <= r_to_w_fifo_cnt - 'd1;
                 end
             end
             else begin
-                wvalid            <= 1'b0;
-            end   
+                wvalid <= 1'b0;  // data not ready to be given
+            end
+   
+            // last flag 
+            if((state == S_BUSY) && (wcnt == 4'd0)) begin
+                wlast <= 1'b1;
+            end 
+            else begin
+                wlast <= 1'b0;
+            end 
 
-            // Wait 
+            // Loop back to RREQ phase or Wait
+            // At this point, a set of writings have been done
+            if((state == S_BUSY) && wlast) begin
+                if(cnt != 0) begin
+                    arvalid <= 1'b1;
+                end
+                else begin
+                    arvalid <= 1'b0;
+                end
+            end
+            else begin // dummy to prevent latch formation
+            end
         end
     end
-   
+    
     /********************************************************/
     /**************** Output Assignments ********************/
     /********************************************************/
@@ -292,7 +315,7 @@ module DMAC_ENGINE
     assign arlen_o      = (cnt >= 'd64) ? 4'hF : cnt[5:2]-4'h1; 
     assign arsize_o     = 3'b010;                                  // 4 bytes per transfer
     assign arburst_o    = 2'b01;                                   // incremental
-    assign arvalid_o    = arvalid; 
+
     // R Channel
     assign rready_o     = rready & !r_to_w_fifo_full; 
     // AW Channel 
